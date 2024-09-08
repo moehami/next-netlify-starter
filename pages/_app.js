811236255c6470c414e8483b163a41ea7c98b109
moehami <m.aljaaly@yahoo.co.uk>
2024-09-09 01:15:23 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '@styles/globals.css'
-
+//import '@styles/globals.css'
+import styles from '../styles/globals.css';
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
