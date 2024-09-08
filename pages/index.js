8cc1a923b637f730eedd6847260f8af0fc48507d
moehami <m.aljaaly@yahoo.co.uk>
2024-09-08 22:05:32 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import styles from '../styles/Home.module.css';
+//import styles from '../styles/Home.module.css';
 export default function Home() {
     const callAPI = async () => {
         try {
