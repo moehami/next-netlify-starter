72a2600d85b3d2ee0c97da91a267bd853465e483
moehami <m.aljaaly@yahoo.co.uk>
2024-09-09 01:11:28 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import '@styles/globals.css'
-//import styles from '../styles/globals.css';
+//import '@styles/globals.css'
+import styles from '../styles/globals.css';
 export default function Home() {
     const callAPI = async () => {
         try {
