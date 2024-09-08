28cd658111672b55b2965fd434be03fd3ba61de6
moehami <m.aljaaly@yahoo.co.uk>
2024-09-08 22:01:45 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,23 @@
 import styles from '../styles/Home.module.css';
 export default function Home() {
-    const callAPI = async () => {};
+    const callAPI = async () => {
+        try {
+            const res = await fetch(
+                `https://famous-quotes4.p.rapidapi.com/random?category=all&count=2`,
+                {
+                    method: 'GET',
+                    headers: {
+                        'X-RapidAPI-Key': 'your-rapidapi-key',
+                        'X-RapidAPI-Host': 'famous-quotes4.p.rapidapi.com',
+                    },
+                }
+            );
+            const data = await res.json();
+            console.log(data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
     return (
         <div className={styles.container}>
             <main className={styles.main}>
